c7fae75b5cc2906a59960bc01ed5fa5a5ddd4b4b
alexandr <alexandr.ter@umbrella.ch>
2022-04-03 23:45:41 +0200

    our own observable

--- src/app/observable-demo/counter.service.ts
+++ src/app/observable-demo/counter.service.ts
@@ -1,8 +1,19 @@
 import {Injectable} from '@angular/core';
+import {Observable, Observer} from "rxjs";
 
 @Injectable()
 export class CounterService {
 
   count = 0;
 
+  observable = new Observable((observer: Observer<number>) => {
+    console.log("we have got a new subscriber!")
+    setInterval(
+      () => {
+        console.log("calling next!")
+        observer.next(this.count);
+      }, 1000);
+
+    return () => console.log("someone just unsubscribed");
+  })
 }
--- src/app/observable-demo/shower/shower.component.ts
+++ src/app/observable-demo/shower/shower.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CounterService} from "../counter.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shower',
@@ -8,14 +9,25 @@ import {CounterService} from "../counter.service";
 export class ShowerComponent implements OnInit {
 
   clickCounter = 0;
+  subscription?: Subscription;
 
   constructor(private counterService: CounterService) {
   }
 
   ngOnInit() {
+
   }
 
-  show() {
-    this.clickCounter = this.counterService.count;
+  subscribe() {
+    this.subscription = this.counterService.observable
+      .subscribe(
+        click => this.clickCounter = click,
+        error => console.log("error occurred " + error),
+        () => console.log("observable completed?")
+      );
+  }
+
+  unsubscribe() {
+    this.subscription?.unsubscribe();
   }
 }
