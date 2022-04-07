43b3eb5da5cc0e75ef2fd7ab8cb8d67129ed7c03
alexandr <alexandr.ter@umbrella.ch>
2022-04-07 13:31:16 +0200

    operators do not modify original observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables-demo';
+export class AppComponent implements OnInit{
+
+  originalObservable = new Observable<number>(observer => {
+    console.log("emitting first value of original observable");
+    observer.next(1);
+    console.log("emitting second value of original observable");
+    observer.next(2);
+    console.log("emitting third value of original observable");
+    observer.next(3);
+  })
+
+  transformedObservable = this.originalObservable.pipe(
+    map(value => {
+      console.log("emitting a value of transformed observable")
+      return value + 10;
+    })
+  )
+
+  ngOnInit(): void {
+    this.originalObservable.subscribe(next => console.log("consuming value = " + next))
+    // this.transformedObservable.subscribe(next => console.log("consuming value = " + next))
+  }
 }
