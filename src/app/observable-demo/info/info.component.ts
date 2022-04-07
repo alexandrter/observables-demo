c3a8f957755ad0ed33e748652678d49e7a0529e9
alexandr <alexandr.ter@umbrella.ch>
2022-04-07 09:35:20 +0200

    Routing unsubscribing
    -does not unsubscribe, but manages to emit when the component exists
    
    Routing unsubscribing
    
    -does not unsubscribe, but manages to emit when the component exists

--- src/app/observable-demo/info/info.component.ts
+++ src/app/observable-demo/info/info.component.ts
@@ -1,17 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent implements OnInit, OnDestroy {
 
+  subscription?: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.activatedRoute.url
-      .subscribe(url => console.log('Observing from InfoComponent. The URL changed to: ' + url));
+    console.log('subscription status: closed=' + this.subscription?.closed);
+    this.subscription = this.activatedRoute.url
+      .subscribe(url => console.log('Observing from InfoComponent. The URL changed to: ' + url))
+      .add(new Subscription(() => console.log("UNSUBSCRIBED!"))); //won't be shown!
+  }
+
+  ngOnDestroy() {
+    console.log('InfoComponent destroyed. No unsubscription performed by component');
   }
 }
